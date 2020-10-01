96a44b70f14aa783e929db08983f9f22c989bb7f
Junel Jig Jimenez <48640698+ziggsjimenez@users.noreply.github.com>
2020-10-01 05:55:41 +0000

    local composer settings

--- public/js/app.js
+++ public/js/app.js
@@ -49839,8 +49839,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\kiosk\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\kiosk\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /var/www/bacmanolo/kiosk/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /var/www/bacmanolo/kiosk/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
